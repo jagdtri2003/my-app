fa36e18ab2305de3553e5ea31987a424f864bf54
Jagdamba Tripathi <jagdtri2003@gmail.com>
2023-08-06 21:04:15 +0530

    new file:   src/components/Navbar.js

---
+++ src/components/MainCard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function MainCard() {
+  return (
+    <>
+    <ul className="nav nav-tabs" id="myTab" role="tablist">
+    <li className="nav-item" role="presentation">
+        <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Flights</button>
+    </li>
+    <li className="nav-item" role="presentation">
+        <button className="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Hotels</button>
+    </li>
+    <li className="nav-item" role="presentation">
+        <button className="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false">Car Rent</button>
+    </li>
+    </ul>
+    <div className="tab-content my-2" id="myTabContent">
+    <div className="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">Flights</div>
+    <div className="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">Hotels</div>
+    <div className="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">Car Rent</div>
+    </div>
+    </>
+  )
+}

1c45258c46ddf73a66fd562340f388bd6dd5144d
Jagdamba Tripathi <jagdtri2003@gmail.com>
2023-08-08 12:59:13 +0530

    Added QRCode in PDF

--- src/components/FlightCard2.js
+++ src/components/FlightCard2.js
@@ -5,57 +5,77 @@ import jsPDF from 'jspdf';
 export default function FlightCard2({ flight, passengerCount,paymentStatus,setPaymentStatus }) {
   const price = flight.price * parseInt(passengerCount);
 
+  function generateReferenceId() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const length = 8; // Set the desired length of the reference ID
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters.charAt(randomIndex);
+    }
+    return result;
+  }
+
   const generatePDF = () => {
     const doc = new jsPDF();
     
-    // Generate a random number
-    const randomNum = Math.floor(Math.random() * 10000);
+    const referenceId = generateReferenceId();
+
+    const qrCodeImageUrl = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(referenceId)}&size=100x100`;
+
+    // Add the QR code image to the PDF
+    const img = new Image();
+    img.src = qrCodeImageUrl;
+    img.onload = () => {
+      doc.addImage(img, 'PNG',130,30,50,50);
     
-    // Add a header with your travel agency name
-    doc.setFontSize(20);
-    doc.setTextColor(52, 58, 64); // Bootstrap's secondary color
-    doc.setFont('courier', 'normal');
-    doc.text('TravelKro - Flight Receipt', 35, 15); // Replace with your travel agency name
+      // Add a header with your travel agency name
+      doc.setFontSize(20);
+      doc.setTextColor(52, 58, 64); // Bootstrap's secondary color
+      doc.setFont('courier', 'normal');
+      doc.text('TravelKro - Flight Receipt', 35, 15); // Replace with your travel agency name
 
-    const titleWidth = doc.getStringUnitWidth('TravelKro - Flight Receipt') * doc.internal.getFontSize() / doc.internal.scaleFactor;
-    const titleX = 35;
-    const titleY = 15;
-    doc.line(titleX, titleY + 2, titleX + titleWidth, titleY + 2);
-    
-    // Add flight details
-    doc.setDrawColor(52, 58, 64); // Set border color to Bootstrap's secondary color
-    doc.rect(5,5,200,100, 'S'); // Draw a border around the flight details area
-    doc.setFontSize(14);
-    doc.setTextColor(33, 37, 41); // Bootstrap's dark color
-    doc.setFont('helvetica', 'normal');
-    doc.text(`Flight Details: ${flight.name} - ${flight.flight_number}`, 10, 30);
-    doc.text(`Departure: ${flight.dep_city}`, 10, 40);
-    doc.text(`Destination: ${flight.arrival_city}`, 10, 50);
-    doc.text(`Departure Time: ${flight.dep_time}`, 10, 60);
-    doc.text(`Passengers: ${passengerCount}`, 10, 70);
-    doc.text(`Total Price:${price} Rs`, 10, 80);
-    doc.text('Payment Status: Successful ',10,90)
+      const titleWidth = doc.getStringUnitWidth('TravelKro - Flight Receipt') * doc.internal.getFontSize() / doc.internal.scaleFactor;
+      const titleX = 35;
+      const titleY = 15;
+      doc.line(titleX, titleY + 2, titleX + titleWidth, titleY + 2);
+      
+      // Add flight details
+      doc.setDrawColor(52, 58, 64); // Set border color to Bootstrap's secondary color
+      doc.rect(5,5,200,100, 'S'); // Draw a border around the flight details area
+      doc.setFontSize(14);
+      doc.setTextColor(33, 37, 41); // Bootstrap's dark color
+      doc.setFont('helvetica', 'normal');
+      doc.text(`Flight Details: ${flight.name} - ${flight.flight_number}`, 10, 30);
+      doc.text(`Departure: ${flight.dep_city}`, 10, 40);
+      doc.text(`Destination: ${flight.arrival_city}`, 10, 50);
+      doc.text(`Departure Time: ${flight.dep_time}`, 10, 60);
+      doc.text(`Passengers: ${passengerCount}`, 10, 70);
+      doc.text(`Total Price:${price} Rs`, 10, 80);
+      doc.text('Payment Status: Successful ',10,90);
+      doc.text(`Reference Id: ${referenceId}`,130,90);
 
-    const note = `
-    Dear Valued Traveler,
+      const note = `
+      Dear Valued Traveler,
 
-    Thank you for choosing TravelKro for your journey. We're excited to be part of your adventure and are committed to making 
-    your experience exceptional.
+      Thank you for choosing TravelKro for your journey. We're excited to be part of your adventure and are committed to making 
+      your experience exceptional.
 
-    May your flight be smooth, your destination unforgettable, and your memories everlasting.
+      May your flight be smooth, your destination unforgettable, and your memories everlasting.
 
-    Safe travels and happy trails!
+      Safe travels and happy trails!
 
-    Warm regards,
-    TravelKro
-  `;
-  doc.setFontSize(10);
-  doc.setTextColor(33, 37, 41);
-  doc.text(note, 5, 110);
-    
-    // Save the PDF with a random number in the name
-    doc.save(`flight_details_${randomNum}.pdf`);
-  };
+      Warm regards,
+      TravelKro
+    `;
+    doc.setFontSize(10);
+    doc.setTextColor(33, 37, 41);
+    doc.text(note, 5, 110);
+      
+      // Save the PDF with a random number in the name
+      doc.save(`flight_details_${referenceId}.pdf`);
+    };
+  }
   
   const handlePayNowClick = () => {
     // Simulate payment processing
--- src/components/HotelCard2.js
+++ src/components/HotelCard2.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import jsPDF from 'jspdf';
 import { Button, Spinner } from 'react-bootstrap';
-
+import axios from 'axios';
 
 export default function HotelCard2({hotel,buttontxt,checkInDate,checkOutDate ,paymentStatus,setPaymentStatus}) {
 
@@ -32,56 +32,77 @@ export default function HotelCard2({hotel,buttontxt,checkInDate,checkOutDate ,pa
     } else if (paymentStatus === 'success') {
       buttonText = 'Payment Successful!';
     }
+    function generateReferenceId() {
+      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      const length = 8; // Set the desired length of the reference ID
+      let result = '';
+      for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        result += characters.charAt(randomIndex);
+      }
+      return result;
+    }
 
     const generatePDF = () => {
+
       const doc = new jsPDF();
       const formattedStartDate = startDate.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
       const formattedEndDate = endDate.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
       
       // Generate a random number
-      const randomNum = Math.floor(Math.random() * 10000);
+      const referenceId = generateReferenceId();
+
+      const qrCodeImageUrl = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(referenceId)}&size=100x100`;
+
+      // Add the QR code image to the PDF
+      const img = new Image();
+      img.src = qrCodeImageUrl;
+      img.onload = () => {
+        doc.addImage(img, 'PNG',130,30,50,50);
       
       // Add a header with your travel agency name
-      doc.setFontSize(20);
-      doc.setTextColor(52, 58, 64); // Bootstrap's secondary color
-      doc.setFont('courier', 'normal');
-      doc.text('TravelKro - Hotel Receipt', 35, 15); // Replace with your travel agency name
-  
-      const titleWidth = doc.getStringUnitWidth('TravelKro - Hotel Receipt') * doc.internal.getFontSize() / doc.internal.scaleFactor;
-      const titleX = 35;
-      const titleY = 15;
-      doc.line(titleX, titleY + 2, titleX + titleWidth, titleY + 2);
-      
-      doc.setDrawColor(52, 58, 64);
-      doc.rect(5,5,200,100, 'S');
-      doc.setFontSize(14);
-      doc.setTextColor(33, 37, 41);
-      doc.setFont('helvetica', 'normal');
-      doc.text(`Hotel Name: ${hotel.name}`, 10, 30);
-      doc.text(`Location: ${hotel.location}`, 10, 40);
-      doc.text(`Number of Days: ${numberOfDays}`, 10, 50);
-      doc.text(`From Date: ${formattedStartDate}`, 10, 60);
-      doc.text(`To Date: ${formattedEndDate}`, 10, 70);
-      doc.text(`Total Price:${total} Rs`, 10, 80);
-      doc.text('Payment Status: Successful ',10,90)
+        doc.setFontSize(20);
+        doc.setTextColor(52, 58, 64); // Bootstrap's secondary color
+        doc.setFont('courier', 'normal');
+        doc.text('TravelKro - Hotel Receipt', 35, 15); // Replace with your travel agency name
+    
+        const titleWidth = doc.getStringUnitWidth('TravelKro - Hotel Receipt') * doc.internal.getFontSize() / doc.internal.scaleFactor;
+        const titleX = 35;
+        const titleY = 15;
+        doc.line(titleX, titleY + 2, titleX + titleWidth, titleY + 2);
+        
+        doc.setDrawColor(52, 58, 64);
+        doc.rect(5,5,200,100, 'S');
+        doc.setFontSize(14);
+        doc.setTextColor(33, 37, 41);
+        doc.setFont('helvetica', 'normal');
+        doc.text(`Hotel Name: ${hotel.name}`, 10, 30);
+        doc.text(`Location: ${hotel.location}`, 10, 40);
+        doc.text(`Number of Days: ${numberOfDays}`, 10, 50);
+        doc.text(`From Date: ${formattedStartDate}`, 10, 60);
+        doc.text(`To Date: ${formattedEndDate}`, 10, 70);
+        doc.text(`Total Price:${total} Rs`, 10, 80);
+        doc.text('Payment Status: Successful ',10,90);
+        doc.text(`Reference Id: ${referenceId}`,130,90);
 
-      const note = `
-      Dear Valued Guest,
-  
-      Thank you for choosing TravelKro for your stay. We're dedicated to making your experience exceptional and memorable.
-  
-      May your days be filled with relaxation, discovery, and delightful moments.
-  
-      Safe travels and wonderful memories,
-      TravelKro
-    `;
-    doc.setFontSize(10);
-    doc.setTextColor(33, 37, 41);
-    doc.text(note,3, 110);
-      
-      // Save the PDF with a random number in the name
-      doc.save(`hotel_details_${randomNum}.pdf`);
-    };
+        const note = `
+        Dear Valued Guest,
+    
+        Thank you for choosing TravelKro for your stay. We're dedicated to making your experience exceptional and memorable.
+    
+        May your days be filled with relaxation, discovery, and delightful moments.
+    
+        Safe travels and wonderful memories,
+        TravelKro
+      `;
+      doc.setFontSize(10);
+      doc.setTextColor(33, 37, 41);
+      doc.text(note,3, 110);
+        
+        // Save the PDF with a random number in the name
+        doc.save(`hotel_details_${referenceId}.pdf`);
+      };
+  };
 
   return (
     <div className="card mb-3">

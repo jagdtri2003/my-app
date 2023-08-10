085e38b88a0d100eb4d6054afffd370912736e2e
Jagdamba Tripathi <jagdtri2003@gmail.com>
2023-08-10 20:58:40 +0530

    added Firebase to App

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAgpgD0HyyaPVme1ckdqaZcR5wujV65tIA",
+  authDomain: "travelkro-1e28f.firebaseapp.com",
+  projectId: "travelkro-1e28f",
+  storageBucket: "travelkro-1e28f.appspot.com",
+  messagingSenderId: "603165283322",
+  appId: "1:603165283322:web:b9710162f8a70121faca43"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

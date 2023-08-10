33d0bc9c8a6784d519011ec44c9c6167208f54f3
Jagdamba Tripathi <jagdtri2003@gmail.com>
2023-08-10 11:23:43 +0530

    added icons

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -9,13 +9,13 @@ export default function MainCard() {
     <>
     <ul className="nav nav-tabs" id="myTab" role="tablist">
     <li className="nav-item" role="presentation">
-        <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Flights</button>
+        <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true"><i class="fa-solid fa-plane-up"></i> Flights</button>
     </li>
     <li className="nav-item" role="presentation">
-        <button className="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Hotels</button>
+        <button className="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false"><i class="fa-solid fa-hotel"></i> Hotels</button>
     </li>
     <li className="nav-item" role="presentation">
-        <button className="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false">Car Rent</button>
+        <button className="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false"><i class="fa-solid fa-car"></i> Car Rent</button>
     </li>
     </ul>
     <div className="tab-content my-2" id="myTabContent">

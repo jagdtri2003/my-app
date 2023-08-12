81bade5f666faad5c19ef1b2fbc99ac54808b607
Jagdamba Tripathi <jagdtri2003@gmail.com>
2023-08-12 10:00:14 +0530

    added security for keys

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
     const firebaseConfig = {
-        apiKey: "AIzaSyAgpgD0HyyaPVme1ckdqaZcR5wujV65tIA",
-        authDomain: "travelkro-1e28f.firebaseapp.com",
-        projectId: "travelkro-1e28f",
-        storageBucket: "travelkro-1e28f.appspot.com",
-        messagingSenderId: "603165283322",
-        appId: "1:603165283322:web:b9710162f8a70121faca43"
-      };
+      apiKey: process.env.REACT_APP_API_KEY,
+      authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+      projectId: process.env.REACT_APP_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+      appId: process.env.REACT_APP_APP_ID,
+    };
       
       // Initialize Firebase
       const app = initializeApp(firebaseConfig);

40a42d2de84181d5b4d755ed2e81f9763f6c0c00
Jagdamba Tripathi <jagdtri2003@gmail.com>
2023-08-18 23:56:53 +0530

    added profile logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,67 @@
-import React from 'react'
-import logo from '../images/travel-kro-logo.png'
-import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const profileLogoUrl = "https://e7.pngegg.com/pngimages/178/595/png-clipart-user-profile-computer-icons-login-user-avatars-monochrome-black-thumbnail.png";
+
 export default function Navbar(props) {
+  const [collapseOpen, setCollapseOpen] = useState(false);
+
+  const handleCollapseToggle = () => {
+    setCollapseOpen(!collapseOpen);
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg " style={{ backgroundColor: '#45526e'}}>
-        <div className="container-fluid">
-        <Link className="navbar-brand" to="/" style={{ color: '#fff' ,fontFamily:'Cherry Bomb One'}}>
-        {/* <img src={logo} alt="Logo" width="110" height="60" class="d-inline-block align-text-top"/> */}
-        {props.title}
-        </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#45526e' }}>
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler order-0"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded={collapseOpen}
+          aria-label="Toggle navigation"
+          onClick={handleCollapseToggle}
+        >
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent" >
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <Link className="navbar-brand mx-auto pe-4 order-1" to="/" style={{ color: '#fff', fontFamily: 'Cherry Bomb One' }}>
+          {props.title}
+        </Link>
+        <div className={`collapse navbar-collapse order-2${collapseOpen ? ' show' : ''}`} id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-            <Link className="nav-link"  aria-current="page" to="/">Home</Link>
+              <Link className="nav-link" aria-current="page" to="/">
+                Home
+              </Link>
             </li>
             <li className="nav-item">
-            <Link className="nav-link" to="/about">About Us</Link>
+              <Link className="nav-link" to="/about">
+                About Us
+              </Link>
             </li>
             <li className="nav-item">
-            <Link className="nav-link" to="/contact">Contact Us</Link>
+              <Link className="nav-link" to="/contact">
+                Contact Us
+              </Link>
             </li>
-        </ul>
-        <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button className="btn btn-outline-light" type="submit">Search</button>
-        </form>
+          </ul>
         </div>
-    </div>
+        {!collapseOpen && (
+          <div className="order-3">
+            <img title="My Profile" src={profileLogoUrl} alt="Profile" style={{ width: '30px', height: '30px',borderRadius: '50%' }} />
+          </div>
+        )}
+      </div>
     </nav>
-  )
+  );
 }
 
-Navbar.propsTypes={
-    title:PropTypes.string.isRequired
-}
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
-Navbar.defaultProps={
-    title:'Set Title Here'
-}
+Navbar.defaultProps = {
+  title: 'Set Title Here',
+};
